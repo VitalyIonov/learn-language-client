ff5a270be5bc094c5951d567c5599f24d70c6f59
vitalyionov <vitalyionov@gmail.com>
2025-08-31 18:35:40 +0200

    правит стили

--- app/routes/category/category.tsx
+++ app/routes/category/category.tsx
@@ -25,7 +25,15 @@ export default function Category() {
   return (
     <PageContent>
       <PageTitle title={categoryData?.name || ""} />
-      <div className="mb-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+      <div
+        className={clsx(
+          "grid grid-cols-1 gap-6",
+          "mb-24",
+          "sm:mb-0",
+          "md:grid-cols-2",
+          "lg:grid-cols-3 lg:gap-8",
+        )}
+      >
         {categoryData ? (
           <CurrentLevelSection level={categoryData?.currentLevel.alias || ""} />
         ) : null}
@@ -38,21 +46,33 @@ export default function Category() {
           />
         ) : null}
       </div>
-      <Button
+      <div
         className={clsx(
-          "inline-flex",
-          "flex items-center gap-2",
-          "px-3 py-1.5",
-          "text-sm/6 font-semibold text-white",
-          "bg-indigo-700",
-          "rounded-md",
-          "shadow-inner",
-          "cursor-pointer",
-          "focus:not-data-focus:outline-none data-disabled:cursor-not-allowed data-disabled:bg-slate-500 data-focus:outline data-focus:outline-white data-hover:bg-indigo-800 data-open:bg-gray-700",
+          "fixed right-0 bottom-0 left-0",
+          "flex justify-center",
+          "mt-8 p-4",
+          "bg-slate-900",
+          "sm:static sm:justify-end",
         )}
       >
-        <Link to="questions">Перейти к изучению</Link>
-      </Button>
+        <Button
+          className={clsx(
+            "inline-flex",
+            "flex items-center gap-2",
+            "px-3 py-1.5",
+            "h-16 w-full",
+            "text-sm/6 font-semibold text-white",
+            "bg-indigo-700",
+            "rounded-md",
+            "shadow-inner",
+            "cursor-pointer",
+            "focus:not-data-focus:outline-none data-disabled:cursor-not-allowed data-disabled:bg-slate-500 data-focus:outline data-focus:outline-white data-hover:bg-indigo-800 data-open:bg-gray-700",
+            "lg:h-auto lg:w-auto",
+          )}
+        >
+          <Link to="questions">Перейти к изучению</Link>
+        </Button>
+      </div>
     </PageContent>
   );
 }
