d0f98134c9188ffa4f13669807cf82093274869e
vitalyionov <vitalyionov@gmail.com>
2025-09-28 16:30:36 +0200

    правит лого и favicon

--- app/shared/components/page-header/page-header.tsx
+++ app/shared/components/page-header/page-header.tsx
@@ -37,11 +37,7 @@ export function PageHeader({ userData }: Props) {
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
+            <img alt="" src="/logo.svg" className="h-10 w-auto" />
           </a>
         </div>
         <div className="flex lg:hidden">
